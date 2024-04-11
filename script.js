a7fb741e924269a574c1e7ec9fe64eed553a9d87
725-AppSoftEng <s224062259@deakin.edu.au>
2024-04-11 22:48:04 +1000

    initial commit

--- script.js
+++ script.js
@@ -1,48 +1,40 @@
-const cardList = [
-    {
-        title: "Snooby",
-        image: "Pup2.jpeg",
-        link: "About Snooby",
-        descripton: "Hello! Hope you have a nice day!"
-    },
-    {
-        title: "Rocky",
-        image: "Pup3.jpeg",
-        link: "About Rocky",
-        descripton: "Hey there! This is Rocky"
-    }
-]
-
-const clickMe = () => {
-    alert("Thanks for clicking me. Have a great day!")
-}
-
-const submitForm = () => {
-    let formData = {};
-    formData.first_Name = $('#first_Name').val();
-    formData.last_Name = $('#last_Name').val();
-    formData.password = $('#password').val();
-    formData.email = $('#email').val();
-    console.log("Form Data Submitted: ", formData);
-}
-const addCards = (items) => {
-    items.forEach(item => {
-        let itemToAppend = '<div class="col s4 center-align">' + '<div class="card"><div class="card-image waves-effect waves-block waves-light "><img class="activator" src="' + item.image + '">' +
-            '</div><div class="card-content">' +
-            '<span class="card-title activator darkslategrey-text text-darken-4">' + item.title +
-            '<i class="material-icons right">more_vert</i></span><p><a href="#">' + item.link + '</a></p></div>' +
-            '<div class="card-reveal">' +
-            '<span class="card-title darkslategrey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span>' +
-            '<p><a>' + item.descripton + '</a></p>' +
-            '</div></div></div>';
-        $("#card-section").append(itemToAppend);
-    });
-}
-$(document).ready(function () {
-    $('.materialboxed').materialbox();
-    $('#formSubmit').click(() => {
-        submitForm();
-    })
-    addCards(cardList);
+$(document).ready(function(){
+ 
     $('.modal').modal();
+
+    $('#clickMeButton').click(function() {
+        $('#modal1').modal('open');
+    });
+
+  
+    $('#formSubmit').click(function() {
+        var firstName = $('#first_name').val();
+        var lastName = $('#last_name').val();
+        var password = $('#password').val();
+        var email = $('#email').val();
+
+        var formData = {
+            firstName: firstName,
+            lastName: lastName,
+            password: password,
+            email: email
+        };
+
+        
+        $.ajax({
+            type: 'POST',
+            url: '/api/cats', 
+            data: formData,
+            success: function(response) {
+                console.log(response); 
+                
+                $('#catPostSuccess').text('Dog post successful');
+            },
+            error: function(error) {
+                console.error('Error:', error); 
+               
+            }
+        });
+        $('#modal1').modal('close');
+    });
 });
--- server.js
+++ server.js
@@ -1,15 +1,72 @@
-var express = require('express');
-var app = express();
-var port = process.env.port || 3000;
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const uri = "mongodb+srv://manasakavali2001:Snooby@adddatabase.wti0wmc.mongodb.net/?retryWrites=true&w=majority&appName=AddDatabase";
+const port = process.env.PORT || 3000;
+let collection;
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname)); 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res)=>{
-    res.render('index.html');
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
-app.listen(port, ()=>{
-    console.log("App listening to: "+port);
+async function runDBConnection() {
+  try {
+    await client.connect();
+    collection = client.db().collection('Cat');
+    console.log("Database connection established");
+    
+    // Once the connection is established, start the server
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (ex) {
+    console.error(ex);
+  }
+}
+
+runDBConnection();
+
+// Define route handlers after the database connection is established
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html'); 
+});
+
+app.get('/api/cats', async (req, res) => {
+  try {
+    // Ensure that the collection is defined before querying it
+    if (!collection) {
+      throw new Error('Collection is not initialized');
+    }
+
+    const cats = await collection.find({}).toArray();
+    res.json(cats);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post('/api/cats', async (req, res) => {
+  try {
+    // Ensure that the collection is defined before inserting data into it
+    if (!collection) {
+      throw new Error('Collection is not initialized');
+    }
+
+    const catData = req.body;
+    await collection.insertOne(catData); 
+    console.log("Dog post successful");
+    res.send("Dog post successful");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
 });
